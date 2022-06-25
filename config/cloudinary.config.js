022a1a8670d178378869f5c24b93b4a2c016bd7a
emmanuelsolis <emma.ruso@outlook.com>
2022-06-25 13:09:46 -0500

    config an adding cloudinary tools

---
+++ config/cloudinary.config.js
@@ -0,0 +1,26 @@
+//vamos a importar 
+const cloudinary = require('cloudinary').v2
+const { CloudinaryStorage} = require('multer-storage-cloudinary')
+const multer = require("multer")
+
+
+//config 
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_NAME, 
+    api_key: process.env.CLOUDINARY_KEY, 
+    api_secret: process.env.CLOUDINARY_SECRET,
+})
+
+//config storage
+
+const  storage = new CloudinaryStorage({
+    //cloudinary configuration
+    cloudinary,
+    params: {
+        folder:"edit user-project",
+        allowed_formats:['jpg', 'png', 'gif', 'svg'],
+    }
+})
+
+module.exports = multer({ storage})
+
